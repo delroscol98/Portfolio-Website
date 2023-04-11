dce9c3bf450cf26362e3eda6f88efa1bae9424ae
Collin Del Rosario <collindelrosario98@gmail.com>
2023-04-11 20:46:08 +1000

    added show-more-show-less functionality to mainButton

---
+++ main.js
@@ -0,0 +1,15 @@
+const toggleText = () => {
+    let points = document.getElementById('points');
+    let mainButton = document.getElementById('mainButton');
+    let hiddenText = document.getElementById('hiddenText');
+
+    if(points.style.display === 'none') {
+        hiddenText.style.display = 'none';
+        points.style.display = 'inline';
+        mainButton.innerHTML = 'About Me';
+    } else {
+        hiddenText.style.display = 'inline';
+        points.style.display = 'none';
+        mainButton.innerHTML = 'Show Less'
+    }
+}
